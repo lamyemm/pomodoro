da39ea82a224dd40f8be6ee003f07f641ef2f1dc
emilie lamy <lamyemilie82@gmail.com>
2019-02-03 23:12:15 +0100

    Updates

---
+++ precache-manifest.429e1dd8eee83e9e091f4f74955ca62a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7823bd9b3e40f5985df",
+    "url": "/pomodoro/static/css/main.f068026e.chunk.css"
+  },
+  {
+    "revision": "a7823bd9b3e40f5985df",
+    "url": "/pomodoro/static/js/main.a7823bd9.chunk.js"
+  },
+  {
+    "revision": "8c633089612a270d805d",
+    "url": "/pomodoro/static/js/1.8c633089.chunk.js"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "9bf9dfbb89c38c2d913bbc340a64aa9a",
+    "url": "/pomodoro/index.html"
+  }
+];
